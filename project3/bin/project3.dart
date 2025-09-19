76fe45f61b53d4203b3706c9fccc7d79a639ba6e
arimbiph4 <andikaputra022005@gmail.com>
2025-09-19 11:21:33 +0700

    fixedlist

---
+++ project3/bin/project3.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:project3/project3.dart' as project3;
+
+void main(List<String> arguments) {
+  
+var fl = List<int>.filled(4,2);
+fl[0] = 1;
+fl[1] = 2;
+fl[2] = 3;
+fl[3] = 4;
+//fl[4] = 5;
+
+stdout.writeln(fl);
+
+  //print('Hello world: ${project3.calculate()}!');
+}
