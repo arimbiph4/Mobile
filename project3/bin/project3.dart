cdac15214d3af7050bb3623f9e92ad098002c453
arimbiph4 <andikaputra022005@gmail.com>
2025-09-19 11:30:26 +0700

    growablelist

--- project3/bin/project3.dart
+++ project3/bin/project3.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:project3/project3.dart' as project3;
 
 void main(List<String> arguments) {
-  
+
 var fl = List<int>.filled(4,2);
 fl[0] = 1;
 fl[1] = 2;
@@ -11,7 +11,10 @@ fl[2] = 3;
 fl[3] = 4;
 //fl[4] = 5;
 
-stdout.writeln(fl);
+var gl=[1,2,3];
+gl.add(18);
+gl.remove(2);
+stdout.writeln(gl);
 
   //print('Hello world: ${project3.calculate()}!');
 }
